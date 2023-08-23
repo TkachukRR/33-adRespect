6cdd4b873b5a90ec546480c5e6528e794774638f
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-23 19:56:12 +0200

    Added markup and styles to offer section

---
+++ scripts/offer.component.js
@@ -0,0 +1,16 @@
+export function getOfferMarkup(data) {
+  const sectionData = data.filter(
+    (section) => section.sectionTitle === "Oferta"
+  )[0];
+  return `
+    <div class="max-w-[1040px] mx-auto">
+        <h2 class="text-48px font-motser tracking--1.44 mb-[32px] text-black leading-115%">
+            <div class="text-12px mb-[16px] tracking--.12 leading-150% text-green">
+                ${sectionData.sectionTitle}
+            </div>
+            ${sectionData.tagline}
+        </h2>
+        <p class="max-w-[700px] tracking--.16 leading-150%">${sectionData.intro}</p>
+    </div>
+    `;
+}
