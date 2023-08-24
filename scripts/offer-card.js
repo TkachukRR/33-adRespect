b446730a5ee3904a0d188951053226e2d6c09279
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-24 02:14:08 +0200

    Added id to offer cards

--- scripts/offer-card.js
+++ scripts/offer-card.js
@@ -2,7 +2,7 @@ export default function getOfferCardMarkup(options) {
   return options
     .map((option) => {
       return `
-        <li class="px-[40px] py-[48px] rounded-[28px] bg-white  text-black ">
+        <li id="${option.id}" class="px-[40px] py-[48px] rounded-[28px] bg-white  text-black ">
             <img src="../images/${option.image}" class="w-[40px] h-[40px] inline-block mb-[34px]"/>
             <h3 class="mb-[12px] text-28px font-motser font-medium tracking--1.4 leading-115%">${option.title}</h3>
             <p class="mb-[63px] text-14px tracking--.14 leading-150%">${option.description}</p>
