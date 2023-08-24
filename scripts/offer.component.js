d8718874e6cf8fdda1e9c45d50e1570ab760fc2c
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-24 02:04:17 +0200

    Changed offer section

--- scripts/offer.component.js
+++ scripts/offer.component.js
@@ -1,19 +1,18 @@
 import getOfferCardMarkup from "./offer-card.js";
+import { updateAccentWords } from "./helpers.js";
 
 export function getOfferMarkup(data) {
-  const sectionData = data.filter(
-    (section) => section.sectionTitle === "Oferta"
-  )[0];
+  const sectionData = data.offer;
   return `
     <div class="max-w-[1040px] mx-auto">
-        <h2 class="font-motser tracking--1.44 mb-[32px] text-black leading-115%">
+        <h2 class="text-48px font-motser tracking--1.44 mb-[32px] text-black leading-115%">
             <div class="text-12px mb-[16px] tracking--.12 leading-150% text-green">
-                ${sectionData.sectionTitle}
+                ${sectionData.title}
             </div>
-            ${sectionData.tagline}
+            ${updateAccentWords(sectionData.tagline, sectionData.accent)}
         </h2>
         <p class="max-w-[700px] tracking--.16 leading-150%">${
-          sectionData.intro
+          sectionData.description
         }</p>
     </div>
     <ul class="flex gap-[64px] mt-[96px]">
