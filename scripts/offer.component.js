16e8679a579fd2c5472be12e51298d648093cefa
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-24 02:12:21 +0200

    Refactored offer section

--- scripts/offer.component.js
+++ scripts/offer.component.js
@@ -1,19 +1,11 @@
 import getOfferCardMarkup from "./offer-card.js";
-import { updateAccentWords } from "./helpers.js";
+import { getMainSectionMarkup } from "./section-markup-maker.js";
 
 export function getOfferMarkup(data) {
   const sectionData = data.offer;
   return `
     <div class="max-w-[1040px] mx-auto">
-        <h2 class="text-48px font-motser tracking--1.44 mb-[32px] text-black leading-115%">
-            <div class="text-12px mb-[16px] tracking--.12 leading-150% text-green">
-                ${sectionData.title}
-            </div>
-            ${updateAccentWords(sectionData.tagline, sectionData.accent)}
-        </h2>
-        <p class="max-w-[700px] tracking--.16 leading-150%">${
-          sectionData.description
-        }</p>
+      ${getMainSectionMarkup(sectionData)}
     </div>
     <ul class="flex gap-[64px] mt-[96px]">
         ${getOfferCardMarkup(sectionData.options)}
---
+++ scripts/section-markup-maker.js
@@ -0,0 +1,17 @@
+import { updateAccentWords } from "./helpers.js";
+
+export function getMainSectionMarkup(sectionData) {
+  return `
+        <h2 class="text-48px font-motser tracking--1.44 mb-[32px] text-black leading-115%">
+          <div id="#${
+            sectionData.id
+          }" class="text-12px mb-[16px] tracking--.12 leading-150% text-green">
+              ${sectionData.title}
+          </div>
+          ${updateAccentWords(sectionData.tagline, sectionData.accent)}
+        </h2>
+        <p class="max-w-[700px] tracking--.16 leading-150%">${
+          sectionData.description
+        }</p>
+  `;
+}
